1edad65e50903d7204009969f8688ab889f2f1a8
Claude Routine <claude-routine@astrobless.dev>
2026-05-07 17:50:13 +0530

    urls

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,14 +1,14 @@
 // ignore: non_constant_identifier_names
 String get OnesignalID => 'da3c28c2-f556-48d3-b1eb-901d4afbe9ba';
 String contactsupportEmail = "support@gmail.com";
-String imgBaseurl = "https://app.astrobless.in/";
+String imgBaseurl = "https://api.astrobless.in/";
 
-String pdfBaseurl = "https://app.astrobless.in/public";
-String termsconditionurl = "https://app.astrobless.in/terms-and-condition";
-String privacyUrl = "https://app.astrobless.in/privacy-and-policy";
+String pdfBaseurl = "https://api.astrobless.in/public";
+String termsconditionurl = "https://api.astrobless.in/terms-and-condition";
+String privacyUrl = "https://api.astrobless.in/privacy-and-policy";
 String appMode = "DEV";
 String storiesIcon = "assets/images/stories_icon.png";
 Map<String, dynamic> appParameters = {
-  "LIVE": {"apiUrl": "https://app.astrobless.in/api/"},
-  "DEV": {"apiUrl": "https://app.astrobless.in/api/"},
+  "LIVE": {"apiUrl": "https://api.astrobless.in/api/"},
+  "DEV": {"apiUrl": "https://api.astrobless.in/api/"},
 };
