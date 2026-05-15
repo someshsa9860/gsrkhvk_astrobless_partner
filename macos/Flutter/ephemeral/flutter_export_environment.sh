cca464e38fc174a59732446c3dedb511b46d224f
Claude Routine <claude-routine@astrobless.dev>
2026-05-15 22:26:37 +0530

    done

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/teja/company/flutter-versions/flutter-3.32.6"
+export "FLUTTER_APPLICATION_PATH=/Users/teja/company/projects/g-austrology/Main/gsrkhvk-astrologer-app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.2.0"
+export "FLUTTER_BUILD_NUMBER=14"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
